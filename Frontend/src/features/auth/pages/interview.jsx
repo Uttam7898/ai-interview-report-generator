faf72dd11706a4ddc0e2e5d2b6bc4d5a69ef1905
Uttam7898 <soniuttamkumar747@gmail.com>
2026-04-06 19:07:21 +0530

    Update interview.jsx

--- Frontend/src/features/auth/pages/interview.jsx
+++ Frontend/src/features/auth/pages/interview.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import '../../../style.scss';
+import '../../../../style.scss';
 import { useInterview } from '../../hooks/useInterview.js'
 import { useNavigate, useParams } from 'react-router'
 
